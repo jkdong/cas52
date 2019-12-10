372c02bf4e17d659b9d55bec529e20dd30f2ae64
dongdb <dongdb@jndzkj.net>
2019-12-10 12:37:48 +0800

    5.2.6初始提交  测试可用

---
+++ src/main/java/cn/jkdong/utils/CustomPasswordEncoder.java
@@ -0,0 +1,10 @@
+package cn.jkdong.utils;
+
+/**
+ * @ClassName: CustomPasswordEncoder
+ * @Auther: dongdb
+ * @Date: 2019/12/10 12:17
+ * @Description:
+ */
+public class CustomPasswordEncoder {
+}
